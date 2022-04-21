daa077c6b899d1041883985e9e75576e09d56faf
BleuXx <nilmar.exe@gmail.com>
2022-04-21 16:26:18 +0200

    Created a better "map" of endpoints at the root endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
 "use strict";
 
-import alcools from "./alcools/index.js";
+import alcohols from "./alcohols/index.js";
 
 const allRoutes = {};
 
 export default async function (fastify, opts) {
   fastify.addHook("onRoute", (routeOptions) => {
-    if (!allRoutes.hasOwnProperty(routeOptions.prefix)) {
-      allRoutes[routeOptions.prefix] = {};
+    let url = routeOptions.url;
+    if (!url.endsWith("/")) {
+      url = `${url}/`;
+    }
+    let pathChunks = url.split("/");
+    pathChunks = pathChunks.splice(0, pathChunks.length);
+
+    let route = allRoutes;
+    for (const [index, pathChunk] of pathChunks.entries()) {
+      const fixedPathChunk = `${pathChunk}/`;
+
+      if (route[fixedPathChunk] === undefined) {
+        if (index >= pathChunks.length - 1) break;
+
+        route[fixedPathChunk] = {};
+      }
+      route = route[fixedPathChunk];
     }
 
-    if (
-      !allRoutes[routeOptions.prefix].hasOwnProperty(routeOptions.routePath)
-    ) {
-      allRoutes[routeOptions.prefix][routeOptions.routePath] = [];
-    }
-
-    allRoutes[routeOptions.prefix][routeOptions.routePath].push(
-      routeOptions.method
-    );
+    route[routeOptions.method] = `${routeOptions.method} ${routeOptions.url}`;
   });
 
   fastify.get("/", async function (request, reply) {
@@ -26,5 +33,5 @@ export default async function (fastify, opts) {
       routes: allRoutes,
     };
   });
-  fastify.register(alcools, { prefix: "alcoholic_drinks" });
+  fastify.register(alcohols, { prefix: "alcoholic_drinks" });
 }
