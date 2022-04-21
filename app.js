46d297a8276de6c64cdc3ed22dfdb06e2225538f
BleuXx <nilmar.exe@gmail.com>
2022-04-21 16:23:43 +0200

    Clean code

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ import support from "./plugins/support.js";
 import routes from "./routes/index.js";
 
 export default async function (fastify, opts) {
-  // fastify.setNotFoundHandler(function (request, reply) {
-  //   // Default not found handler with preValidation and preHandler hooks
-  // });
-
   fastify.register(sensible);
   fastify.register(support);
   fastify.register(fastifyCors);
